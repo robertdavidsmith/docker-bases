a7b4a8e613266304ce5968f1f98c90af4a54a1b8
Robert Smith <robertdavidsmith@yahoo.com>
2018-09-05 08:54:44 +0100

    Refactor

--- dotnet/build-and-save.sh
+++
@@ -1,5 +0,0 @@
-IMAGE=dotnet-krb:2.1.0-aspnetcore-runtime-alpine
-DESTINATION_FILE=/tmp/`echo $IMAGE | tr : _`.tar
-sudo docker build . -t $IMAGE
-sudo docker save $IMAGE > $DESTINATION_FILE
-gzip -fv $DESTINATION_FILE
