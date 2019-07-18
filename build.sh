1421ebdeb3b7ccd9b32833160f3bcb8c0553950a
GaborA <gabor.acskurucz@gresearch.co.uk>
2019-07-18 14:33:03 +0100

    update build files to give public read access

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -e
 
-if [ $# -ne 4 ]
+if [ $# -ne 3 ]
 then
-    echo "usage: dockerfile_dir output_image_name output_image_version grant_bucket_read_to"
+    echo "usage: dockerfile_dir output_image_name output_image_version"
     exit 1
 fi
 
 DOCKERFILE_DIR=$1
 OUTPUT_IMAGE_NAME=$2
 OUTPUT_IMAGE_VERSION=$3
-BUCKET_READ_PERMS=$4
 
 OUTPUT_IMAGE=${OUTPUT_IMAGE_NAME}:${OUTPUT_IMAGE_VERSION}
 TAR_FILE=${OUTPUT_IMAGE_NAME}${OUTPUT_IMAGE_VERSION}.tar
@@ -21,14 +20,15 @@ docker build -t ${OUTPUT_IMAGE} ${DOCKERFILE_DIR}
 echo Docker save...
 docker save -o  ${TAR_FILE} ${OUTPUT_IMAGE}
 
-echo Create S3 bucket...
+echo Creating S3 bucket ${BUCKET_NAME}...
 aws s3api create-bucket --bucket ${BUCKET_NAME} --acl private
 
-echo Set S3 bucket perms...
-aws s3api put-bucket-acl --bucket ${BUCKET_NAME} --grant-read ${BUCKET_READ_PERMS}
-
 echo Upload to S3 bucket...
-aws s3api put-object --bucket ${BUCKET_NAME} --body ${TAR_FILE} --key $(basename ${TAR_FILE}) --grant-read ${BUCKET_READ_PERMS}
+KEY=$(basename ${TAR_FILE})
+aws s3api put-object --bucket ${BUCKET_NAME} --body ${TAR_FILE} --key ${KEY}
+
+echo Give public read perms on bucket...
+aws s3api put-object-acl --bucket ${BUCKET_NAME} --key ${KEY} --acl public-read
 
 echo Done
 
---
+++ dotnet2.2.1-aspnetcore-runtime-curl/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+$(dirname $0)/../build.sh $(dirname $0) dotnet 2.2.1-aspnetcore-runtime-curl-7.46.0
